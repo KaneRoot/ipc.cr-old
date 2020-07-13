90e98c328a44b6cc7398b8b9fe9d9a9e36db444f
Karchnu <karchnu@karchnu.fr>
2020-07-13 18:40:21 +0200

    IPC::Client has a server_fd property.

--- src/ipc/client.cr
+++ src/ipc/client.cr
@@ -1,13 +1,19 @@
 
 class IPC::Client < IPC::Context
+	property server_fd : Int32
+
 	# By default, this is a client.
 	def initialize(service_name : String)
 		super()
-		r = LibIPC.ipc_connection(self.pointer, service_name)
+		serverfd = 0
+
+		r = LibIPC.ipc_connection(self.pointer, service_name, pointerof(serverfd))
 		if r.error_code != 0
 			m = String.new r.error_message.to_slice
 			raise Exception.new "error during connection establishment: #{m}"
 		end
+
+		@server_fd = server_fd
 
 		# Very important as there are filesystem side-effects.
 		at_exit { close }
